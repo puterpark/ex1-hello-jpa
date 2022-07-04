f79ffab2416f80d1344de5433aee28994b50af32
Puter <puterpark@me.com>
2022-07-04 22:20:05 +0900

    Hello JPA - 애플리케이션 개발

--- README.md
+++ README.md
@@ -19,3 +19,28 @@ JPA는 특정 RDB에 종속되지 않음
 `방언(dialect)` : SQL 표준을 지키지 않는 특정 데이터베이스 만의 고유한 기능  
 `hibernate.dialect` : 해당 속성에 지정 가능
 
+---
+
+## Hello JPA - 애플리케이션 개발
+
+### JPA 구동 방식
+`Persistence`가 설정 정보 조회하고 `EntityManagerFactory`를 생성  
+`EntityManagerFactory`는 `EntityManager`를 생성
+
+`@Entity` : JPA가 관리할 객체  
+`@Id` : 데이터베이스 테이블 PK와 매핑
+
+`EntityManagerFactory` : 하나만 생성해서 어플리케이션 전체에서 공유  
+`EntityManager` : 쓰레드 간에 공유하지 않음 (사용하고 버림)
+
+> JPA의 모든 데이터 변경은 **트랜잭션** 안에서 실행
+
+### JPQL
+JPA는 SQL을 추상화한 **객체 지향 쿼리** 언어 제공
+
+모든 DB 데이터를 객체로 변환해서 검색하는 것은 **불가능**  
+검색을 할 떄 테이블이 아닌 **엔티티 객체**를 대상으로 검색
+
+`JPQL` : **엔티티 객체**를 대상으로 쿼리  
+`SQL` : **데이터베이스 테이블**을 대상으로 쿼리
+
---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,61 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+	public static void main(String[] args) {
+		// 엔티티 매니저 팩토리는 하나만 생성해서 어플리케이션 전체에서 공유
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		// 쓰레드 간에 공유하지 않음 (사용하고 버림)
+		EntityManager em = emf.createEntityManager();
+
+		// JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		try {
+			// 회원 등록
+//			Member member = new Member();
+//			member.setId(2L);
+//			member.setName("HelloB");
+//			em.persist(member);
+
+			// 회원 조회
+//			Member findMember = em.find(Member.class, 1L);
+//			System.out.println("findMember.id = " + findMember.getId());
+//			System.out.println("findMember.name = " + findMember.getName());
+
+			// 회원 삭제
+//			Member findMember = em.find(Member.class, 1L);
+//			em.remove(findMember);
+
+			// 회원 수정
+//			Member findMember = em.find(Member.class, 1L);
+//			findMember.setName("helloJPA"); // List처럼 set만 해도 수정됨
+
+			// JPQL
+			List<Member> result = em.createQuery("select m from Member as m", Member.class)
+					.setFirstResult(0)
+					.setMaxResults(5)
+					.getResultList();
+
+			for (Member member : result) {
+				System.out.println("member.name = " + member.getName());
+			}
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+
+		emf.close();
+	}
+
+}
